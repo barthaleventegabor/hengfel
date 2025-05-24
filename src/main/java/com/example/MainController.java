0f8f2af1b7a6254305e13e394fefc2b726a7b09f
Bartha Levente <b.levi.levi@gmail.com>
2025-05-24 16:36:35 +0200

    Projekt alapja

---
+++ src/main/java/com/example/MainController.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class MainController {
+
+    @FXML
+    private TextField heightField;
+
+    @FXML
+    private TextField radiusField;
+
+    @FXML
+    private TextField surfaceAreaField;
+
+    @FXML
+    void onClickAboutButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickCalcButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClilckExitButton(ActionEvent event) {
+
+    }
+
+}
